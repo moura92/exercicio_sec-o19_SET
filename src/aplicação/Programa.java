c033283321f77138a3949f5517d231e0888d8082
Alisson de Moura <alisson_moura_rj@hotmail.com>
2023-09-04 21:17:08 -0300

    inicio

---
+++ src/aplicação/Programa.java
@@ -0,0 +1,20 @@
+package aplicação;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entidades.Codigo;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		Set<Codigo> cd = new HashSet<>();
+
+		sc.close();
+	}
+
+}
---
+++ src/entidades/Codigo.java
@@ -0,0 +1,38 @@
+package entidades;
+
+import java.util.Objects;
+
+public class Codigo {
+
+	private Integer codigo;
+
+	public Codigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Codigo other = (Codigo) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+
+}
